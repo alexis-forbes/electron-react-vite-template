15da5aae895acff38c98ab33f66557e4a86a8884
alexis-forbes <a.forbes.gil@gmail.com>
2025-11-18 18:01:53 +0100

    fix: e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,12 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests/e2e",
+  webServer: {
+    command: "npm run dev:renderer",
+    url: "http://localhost:5173",
+    reuseExistingServer: !process.env.CI,
+    timeout: 120_000
+  },
   use: {
     headless: true
   },
--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -7,5 +7,5 @@ import { expect, test } from "@playwright/test";
 test("homepage renders", async ({ page }) => {
   await page.goto("http://localhost:5173/");
 
-  await expect(page).toHaveTitle(/Electron React Vite Template/i);
+  await expect(page).toHaveTitle("Hello World!");
 });
