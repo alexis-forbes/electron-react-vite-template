1353ca7fec9be4db1f40f140dcc9c93eddcc76cf
alexis-forbes <a.forbes.gil@gmail.com>
2025-11-19 08:36:28 +0100

    feat: adding dbDemo in global types

--- src/preload/types/global.d.ts
+++ src/preload/types/global.d.ts
@@ -1,7 +1,16 @@
+import type { DbDemoSyncResult } from "../../main/services/dbDemoService";
+import type { DbDemoNote } from "../../shared/types/dbDemo";
 import type { TcpMessage } from "../../shared/types/tcp";
+
+export interface DbDemoApi {
+  getNotes: () => Promise<DbDemoNote[]>;
+  addNote: (text: string) => Promise<DbDemoNote>;
+  sync: () => Promise<DbDemoSyncResult>;
+}
 
 export interface WindowApi {
   onTcpMessage: (handler: (message: TcpMessage) => void) => void;
+  dbDemo: DbDemoApi;
 }
 
 declare global {
